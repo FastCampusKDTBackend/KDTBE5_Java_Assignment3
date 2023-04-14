486dbb7b0b66c25ff1ded03155e03be580156e0b
yongho shin <gdtknight@naver.com>
2023-04-14 19:33:00 +0900

    Add Enums
    Electronic 클래스에서 사용되는 Enum 정의 (본인인증 방법, 제조 회사명)

---
+++ src/main/java/me/day05/practice/practice01/CompanyName.java
@@ -0,0 +1,13 @@
+package me.day05.practice.practice01;
+
+/**
+ * 제조 회사명 - 삼성, 엘지, 애플
+ * <p>
+ * @author YongHo Shin
+ * @since v0.1 (2023-04-12)
+ */
+public enum CompanyName {
+  SAMSUNG,
+  LG,
+  APPLE
+}
