7915725847a2ff1e7c0d5ddf651f64ecb1e90fc0
brotherstone97 <brotherstone97@gmail.com>
2023-04-16 23:44:59 +0900

    solve practice1

---
+++ src/main/java/practice1/AuthMethod.java
@@ -0,0 +1,8 @@
+package practice1;
+
+public enum AuthMethod {
+    PIN,
+    PATTERN,
+    FACE_ID,
+    FINGERPRINT,
+}
---
+++ src/main/java/practice1/Electronic.java
@@ -0,0 +1,88 @@
+package practice1;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Electronic {
+    private Long productNo;
+    private String modelName;
+    private CompanyName companyName;
+    private LocalDate dateOfMade;
+    private AuthMethod[] authMethod;
+
+    public Electronic(Long productNo, String modelName, CompanyName companyName, LocalDate dateOfMade, AuthMethod[] authMethod) {
+        this.productNo = productNo;
+        this.modelName = modelName;
+        this.companyName = companyName;
+        this.dateOfMade = dateOfMade;
+        this.authMethod = authMethod;
+    }
+
+    public Long getProductNo() {
+        return productNo;
+    }
+
+    public void setProductNo(Long productNo) {
+        this.productNo = productNo;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public CompanyName getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(CompanyName companyName) {
+        this.companyName = companyName;
+    }
+
+    public LocalDate getDateOfMade() {
+        return dateOfMade;
+    }
+
+    public void setDateOfMade(LocalDate dateOfMade) {
+        this.dateOfMade = dateOfMade;
+    }
+
+    public AuthMethod[] getAuthMethod() {
+        return authMethod;
+    }
+
+    public void setAuthMethod(AuthMethod[] authMethod) {
+        this.authMethod = authMethod;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Electronic that = (Electronic) o;
+        return Objects.equals(productNo, that.productNo) && Objects.equals(modelName, that.modelName) && companyName == that.companyName && Objects.equals(dateOfMade, that.dateOfMade) && Arrays.equals(authMethod, that.authMethod);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(productNo, modelName, companyName, dateOfMade);
+        result = 31 * result + Arrays.hashCode(authMethod);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Electronic{" +
+                "productNo=" + productNo +
+                ", modelName='" + modelName + '\'' +
+                ", companyName=" + companyName +
+                ", dateOfMade=" + dateOfMade +
+                ", authMethod=" + Arrays.toString(authMethod) +
+                '}';
+    }
+}
+
