020977765a299d67da8b590410b366f80ddb6f0f
WooSeok77 <wi8tkf@naver.com>
2023-04-17 05:03:46 +0900

    main fix

--- me/day05/practice/practice01/Main.java
+++ me/day05/practice/practice01/Main.java
@@ -4,9 +4,9 @@ package me.day05.practice.practice01;
 
 public class Main {
     public static void main(String[] args) {
-        Electronic a1 = new Electronic("javis", Electronic.CompanyName.SAMSUNG, "20000120", new Electronic.AuthMethod[]{Electronic.AuthMethod.PIN});
+        Electronic a1 = new Electronic("javis", Company.SAMSUNG, "20000120", new AuthMethod[]{AuthMethod.PIN});
         System.out.println(a1.toString());
-        Electronic a2 = new Electronic("havis", Electronic.CompanyName.SAMSUNG, "20000121", new Electronic.AuthMethod[]{Electronic.AuthMethod.PIN});
+        Electronic a2 = new Electronic("havis", Company.SAMSUNG, "20000121", new AuthMethod[]{AuthMethod.PIN});
         System.out.println(a2.toString());
 
         User m1 = new User("fallen", "page7", "010-2222-3333", "ast@gmail.com", "060220", new String[]{"hell"});
