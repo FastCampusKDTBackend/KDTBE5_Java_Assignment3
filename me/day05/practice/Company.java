e0c48d40cda7034ad44d0eb69ea738958cdb1667
LeeJaewon <jaewon971111@gmail.com>
2023-04-17 23:00:24 +0900

    Java Assignment3 upload by LeeJaewon

---
+++ me/day05/practice/Company.java
@@ -0,0 +1,5 @@
+package me.day05.practice;
+
+public enum Company {
+	SAMSUNG, LG, APPLE
+}
---
+++ me/day05/practice/Electronics.java
@@ -0,0 +1,48 @@
+package me.day05.practice;
+
+import java.util.Arrays;
+
+public class Electronics {
+	private static Electronics electronics;
+	private Electronic[] electronicList;
+
+	private Electronics() {
+
+	}
+
+	public static Electronics getInstance() {
+		if (electronics == null) {
+			electronics = new Electronics();
+		}
+		return Electronics.electronics;
+	}
+
+	public Electronic findByProductNo(String productNo) {
+		return Arrays.stream(this.electronicList)
+				.filter(electronic -> electronic.getProductNo().equals(productNo))
+				.findFirst()
+				.orElse(null);
+	}
+
+	Electronic[] groupByCompanyName(Company company) {
+		return Arrays.stream(this.electronicList)
+				.filter(electronic -> electronic.getCompanyName().equals(company))
+				.toArray(Electronic[]::new);
+	}
+
+	Electronic[] groupByAuthMethod(AuthMethod authMethod) {
+		return Arrays.stream(this.electronicList)
+				.filter(
+						electronic -> Arrays.stream(electronic.getAuthMethods())
+								.anyMatch(authMethods -> authMethods.equals(authMethod))
+				).toArray(Electronic[]::new);
+	}
+
+	public Electronic[] getElectronicList() {
+		return electronicList;
+	}
+
+	public void setElectronicList(Electronic[] electronicList) {
+		this.electronicList = electronicList;
+	}
+}
