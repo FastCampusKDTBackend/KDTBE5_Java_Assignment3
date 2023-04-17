0ee47c798f96a2ccaeb53e6503ca56d14b227314
yongho shin <gdtknight@naver.com>
2023-04-17 22:25:02 +0900

    refactor Electronic
    - 일련번호 생성을 위한 static 변수명 변경 (totalProduct -> seqNum)
    - 인증 방법 배열 변수 이름 복수형으로 변경 (authMethod -> authMethods)
    - 일련번호 생성 부분 별도 메서드로 분리 및 synchronized 키워드 추가
    - 인증 방법 지원 여부 제공 메서드 추가
    - 생성자 private 변경 및 팩토리 메서드 추가
    - hashCode() 좀 더 간단하게 수정

--- src/main/java/me/day05/practice/practice01/Electronic.java
+++ src/main/java/me/day05/practice/practice01/Electronic.java
@@ -1,44 +1,87 @@
 package me.day05.practice.practice01;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * 전자기기 정보
- * <p>
  * - 일련번호 : 변경 불가. 등록일(yyMMdd) + 등록순서(4자리).
- * - 모델명 : 변경 불가.
- * - 제조사 : 변경 불가.
  * - 생산일자 : 변경 불가.
+ * - 모델명
+ * - 제조사
  * - 본인인증 방법 : 여러 방법 동시 사용 가능.
  *
  * @author YongHo Shin
- * @since v0.1 (2023-04-12)
- *
+ * @version 0.2
+ * @since 2023-04-12
  */
 public class Electronic {
-  private static Long totalProduct = 0L;
+  private static Long seqNum = 0L;
   private final Long productNo;
-  private final String modelName;
-  private final CompanyName companyName;
   private final LocalDate dateOfMade;
-  private AuthMethod[] authMethod;
+  private String modelName;
+  private CompanyName companyName;
+  private AuthMethod[] authMethods;
   
-  public Electronic(
+  private Electronic(
       String modelName,
       CompanyName companyName,
-      LocalDate dateOfMade,
-      AuthMethod[] authMethod) {
-    this.productNo = Long.parseLong(
-        LocalDate.now().format(DateTimeFormatter.ofPattern("uuMMdd"))
-            + String.format("%04d", ++totalProduct)
-    );
+      AuthMethod[] authMethods) {
+    this.productNo = generateProductNo();
     this.modelName = modelName;
     this.companyName = companyName;
+    this.dateOfMade = LocalDate.now(ZoneId.systemDefault());
+    this.authMethods = Arrays.copyOf(authMethods, authMethods.length);
+  }
+  
+  private Electronic(
+      Long productNo,
+      LocalDate dateOfMade,
+      String modelName,
+      CompanyName companyName,
+      AuthMethod[] authMethods
+  ) {
+    this.productNo = productNo;
     this.dateOfMade = dateOfMade;
-    this.authMethod = authMethod;
+    this.modelName = modelName;
+    this.companyName = companyName;
+    this.authMethods = Arrays.copyOf(authMethods, authMethods.length);
+  }
+  
+  public static Electronic createWithDetails(
+      String modelName,
+      CompanyName companyName,
+      AuthMethod[] authMethod) {
+    return new Electronic(
+        modelName,
+        companyName,
+        authMethod
+    );
+  }
+  
+  public static Electronic copyOf(Electronic electronic) {
+    return new Electronic(
+        electronic.getProductNo(),
+        electronic.getDateOfMade(),
+        electronic.getModelName(),
+        electronic.getCompanyName(),
+        electronic.getAuthMethods()
+    );
+  }
+  
+  /**
+   * 동시에 여러 제품을 등록할 때 Race Condition 방지 필요
+   *
+   * @return 등록일련번호
+   */
+  private static synchronized long generateProductNo() {
+    return Long.parseLong(
+        LocalDate.now(ZoneId.systemDefault())
+            .format(DateTimeFormatter.ofPattern("uuMMdd"))
+            + String.format("%04d", ++seqNum)
+    );
   }
   
   public Long getProductNo() {
@@ -57,27 +100,52 @@ public class Electronic {
     return dateOfMade;
   }
   
-  public AuthMethod[] getAuthMethod() {
-    return authMethod;
+  public AuthMethod[] getAuthMethods() {
+    return authMethods;
   }
   
-  public void setAuthMethod(AuthMethod[] authMethod) {
-    this.authMethod = authMethod;
+  public void setModelName(String modelName) {
+    this.modelName = modelName;
   }
   
+  public void setCompanyName(CompanyName companyName) {
+    this.companyName = companyName;
+  }
+  
+  public void setAuthMethods(AuthMethod[] authMethods) {
+    this.authMethods = authMethods;
+  }
+  
+  /**
+   * @param authMethod 인증 방법
+   * @return 해당 인증 방법 지원 여부
+   */
+  public boolean supportAuthMethod(AuthMethod authMethod) {
+    return Arrays.stream(authMethods)
+        .anyMatch(registeredMethod->registeredMethod==authMethod);
+  }
+  
+  /**
+   * @return 등록번호, 제조회사, 생산일에 기반하여 계산된 고유 정수값
+   */
   @Override
   public int hashCode() {
-    return Objects.hash(productNo, modelName, companyName);
+    int result = getDateOfMade().hashCode();
+    result = 31 * result + getProductNo().hashCode();
+    result = 31 * result + (getCompanyName() != null ? getCompanyName().hashCode() : 0);
+    return result;
   }
   
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
+    
     Electronic that = (Electronic) o;
-    return productNo.equals(that.productNo)
-        && modelName.equals(that.modelName)
-        && companyName == that.companyName;
+    
+    if (!getDateOfMade().equals(that.getDateOfMade())) return false;
+    if (!getProductNo().equals(that.getProductNo())) return false;
+    return getCompanyName() == that.getCompanyName();
   }
   
   @Override
@@ -87,7 +155,7 @@ public class Electronic {
         ", companyName=" + companyName +
         ", dateOfMade=" + dateOfMade +
         ", modelName='" + modelName + '\'' +
-        ", authMethod=" + Arrays.toString(authMethod) +
+        ", authMethod=" + Arrays.toString(authMethods) +
         '}';
   }
 }
