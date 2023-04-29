d88bc5d9b31bbb8726681eaade594b0060112855
brotherstone97 <brotherstone97@gmail.com>
2023-04-29 20:28:20 +0900

    test: add test for findByProductNo()

--- src/test/java/practice1/ElectronicTest.java
+++ src/test/java/practice1/ElectronicTest.java
@@ -1,28 +1,44 @@
 package practice1;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class ElectronicTest {
-    Electronic electronic;
+    static Electronic electronic;
+    static Electronics electronics;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() {
         electronic = new Electronic(
                 "SM-1000",
                 Company.SAMSUNG,
                 LocalDate.of(2023, 04, 17),
                 new AuthMethod[]{AuthMethod.PIN, AuthMethod.PATTERN});
+        electronics = Electronics.getInstance();
+        electronics.setElectronicList(new Electronic[]{electronic});
     }
 
     @Test
     void createProductNo() {
         Long productNo = electronic.getProductNo();
         assertThat(productNo).isEqualTo(2304170001L);
+    }
+
+    @Test
+    void findExistentProductNo() {
+        Electronic found = electronics.findByProductNo(2304170001L);
+        assertThat(found.getProductNo()).isEqualTo(2304170001L);
+    }
+
+    @Test
+    void findNonexistentProductNo() {
+        assertThatThrownBy(() -> {electronics.findByProductNo(2311132L);})
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
