d845434a6d75730b4721a4c4f0eb823ea7041743
GyuChangSeong <ahdzlq12@gmail.com>
2023-05-02 14:34:34 +0900

    create class Defines for define enum

---
+++ src/me/day05/practice/Defines.java
@@ -0,0 +1,17 @@
+package me.day05.practice;
+
+public class Defines {
+
+    public enum CompanyName{
+        SAMSUNG,
+        LG,
+        APPLE
+    }
+    public enum AuthMethod{
+        Fingerprint,
+        Pattern,
+        Pin,
+        Face
+    }
+
+}
