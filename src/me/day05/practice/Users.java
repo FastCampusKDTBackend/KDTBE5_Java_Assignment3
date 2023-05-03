73057977a00b30e36df0de94527546031edc4598
GyuChangSeong <ahdzlq12@gmail.com>
2023-05-03 14:58:51 +0900

    [2차]Fix 'Users' class. 'userList' naming,'findByUserId' method
    -User[] userList -> users 네이밍 정정했습니다
    -instance변수 위치 조정 하였습니다.
    -findByUserId메서드 for문에서 stream사용 하는 방식으로 수정하였고, optional을 사용하였습니다.
     해당하는 user가 없을경우 호출 하는 곳에서 예외 처리 하도록 throw하였습니다.

--- src/me/day05/practice/Users.java
+++ src/me/day05/practice/Users.java
@@ -1,23 +1,31 @@
 package me.day05.practice;
 
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Users {
 
-    private User[] userList; //필드 - User[] userList (생성된 User 객체들을 모두 저장) -> user객체는 다른 곳에서 생성되는걸 가정하는 것이고 여기서는 따로 안해도 되는건가요??
+    private static Users instance;
+    private User[] users; //필드 - User[] userList (생성된 User 객체들을 모두 저장) -> user객체는 다른 곳에서 생성되는걸 가정하는 것이고 여기서는 따로 안해도 되는건가요??
 
     private Users(){}
 
     public static Users getInstance() {
         if (instance == null)
             instance = new Users();
+
         return instance;
-    } private static Users instance;
+    }
 
     @Override
     public String toString() {
         return "Users{" +
-                "userList=" + Arrays.toString(userList) +
+                "userList=" + Arrays.toString(users) +
                 '}';
     }
 
@@ -26,20 +34,20 @@ public class Users {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Users users = (Users) o;
-        return Arrays.equals(userList, users.userList);
+        return Arrays.equals(this.users, users.users);
     }
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(userList);
+        return Arrays.hashCode(users);
     }
 
     public User[] getUserList() {
-        return userList;
+        return users;
     }
 
     public void setUserList(User[] userList) {
-        this.userList = userList;
+        this.users = userList;
     }
 
     public static void setInstance(Users instance) {
@@ -47,11 +55,17 @@ public class Users {
     }
 
     public User findByUserId(String UserId){
-        for(int i =0; i < userList.length; i++) {
-            if(userList[i].getUserId() == UserId)
-                return userList[i];
-        }
-        return null;
+        //userid가 중복이 없게 처리 해야 될듯
+        //스트림에서 컬렉션으로 집계 후, get(0) 이런식으로 사용을 해도 되나??
+        //user를 get으로 바로 얻어 오면 얕은 복사??
+        //optional로 해도 어차피 trycatch를 사용해야 되나요??
+        User user = Arrays.stream(users).filter(u -> u.getUserId() == UserId).collect(Collectors.toList()).get(0);
+        Optional<User> optionalUser = Optional.ofNullable(user);
+
+        if(!optionalUser.isPresent())
+            throw new NullPointerException();
+
+        return optionalUser.get();
     }
 
     public User copy(User user){
